3526120c2dea35ab6cfcb9d01d852a306053dd04
Robin Tapiru <robintapiru0894@gmail.com>
2024-06-24 17:05:51 +0400

    70 - submit component form datas if else setimeout array methods

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // -- Counter Component --
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
+const formEl = document.querySelector(".form");
+const feebackListEl = document.querySelector(".feedbacks");
+const submitBtnEl = document.querySelector(".submit-btn");
 
 const inputHandler = () => {
   // Determine maximum number of charaters
@@ -19,7 +22,6 @@ const inputHandler = () => {
 textareaEl.addEventListener("input", inputHandler);
 
 // -- Submit Component --
-const formEl = document.querySelector(".form");
 
 const submitHandler = (event) => {
   // preven default broser action submit
@@ -29,14 +31,59 @@ const submitHandler = (event) => {
   const text = textareaEl.value;
 
   //validate text (ex. check if is #hastag i present and text is long enoiugh)
-
   if (text.includes("#") && text.length >= 5) {
-    formEl.classList.add("form--valid");
+    //show valid indicator
+    formEl.classList.add("form--valid"); //remove visual indicator
     setTimeout(() => formEl.classList.remove("form--valid"), 2000);
   } else {
+    //show invalid indicator
     formEl.classList.add("form--invalid");
+    //remove visual indicator
     setTimeout(() => formEl.classList.remove("form--invalid"), 2000);
+
+    //focus textarea
+    textareaEl.focus();
+
+    // stop function execution
+    return;
   }
+
+  // we have text, now extract other info from text
+  const hashtag = text.split(" ").find((word) => word.includes("#"));
+  const company = hashtag.substring(1);
+  const badgeLetter = company.substring(0, 1).toUpperCase();
+  const upvoteCount = 0;
+  const daysAgo = 0;
+
+  // new feeback item html
+  const feebackItemHTML = `
+    <li class="feedback">
+      <button class="upvote">
+          <i class="fa-solid fa-caret-up upvote__icon"></i>
+          <span class="upvote__count">${upvoteCount}</span>
+      </button>
+      <section class="feedback__badge">
+          <p class="feedback__letter">${badgeLetter}</p>
+      </section>
+      <div class="feedback__content">
+          <p class="feedback__company">${company}</p>
+          <p class="feedback__text">${text}</p>
+      </div>
+      <p class="feedback__date">${daysAgo === 0 ? "NEW" : `${daysAgo}d`}</p>
+  </li>
+  `;
+
+  //insert new feedback item in list
+  feebackListEl.insertAdjacentHTML("beforeend", feebackItemHTML);
+
+  //clear textare
+  textareaEl.value = "";
+
+  //blur submit button
+  submitBtnEl.blur();
+
+  //reset counter
+  counterEl.textContent = 150;
 };
 
 formEl.addEventListener("submit", submitHandler);
