b6cf17b7d18b213d268c9360a88611f74cb30331
Robin Tapiru <robintapiru0894@gmail.com>
2024-06-25 16:51:12 +0400

    Feedback List Component Expand Upvote Event Delegation Bubbling

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
-// -- Counter Component --
+const MAX_CHARS = 150;
+const BASE_API_URL = "https://bytegrad.com/course-assets/js/1/api";
+
 const textareaEl = document.querySelector(".form__textarea");
 const counterEl = document.querySelector(".counter");
 const formEl = document.querySelector(".form");
 const feebackListEl = document.querySelector(".feedbacks");
 const submitBtnEl = document.querySelector(".submit-btn");
+const spinnerEl = document.querySelector(".spinner");
 
+const renderFeedbackItem = (feedbackItem) => {
+  // new feeback item html
+  const feebackItemHTML = `
+  <li class="feedback">
+    <button class="upvote">
+      <i class="fa-solid fa-caret-up upvote__icon"></i>
+      <span class="upvote__count">${feedbackItem.upvoteCount}</span>
+    </button>
+    <section class="feedback__badge">
+      <p class="feedback__letter">${feedbackItem.badgeLetter}</p>
+    </section>
+    <div class="feedback__content">
+      <p class="feedback__company">${feedbackItem.company}</p>
+      <p class="feedback__text">${feedbackItem.text}</p>
+    </div>
+    <p class="feedback__date">${
+      feedbackItem.daysAgo === 0 ? "NEW" : `${feedbackItem.daysAgo}d`
+    }</p>
+  </li>
+  `;
+
+  //insert new feedback item in list
+  feebackListEl.insertAdjacentHTML("beforeend", feebackItemHTML);
+};
+
+// -- Counter Component --
 const inputHandler = () => {
   // Determine maximum number of charaters
-  const maxNrChars = 150;
 
   // determin number of charaters currently typed
   const nrCharsTyped = textareaEl.value.length;
 
   // calculate number of charaters left(max minus currently typed)
-  const charsLeft = maxNrChars - nrCharsTyped;
+  const charsLeft = MAX_CHARS - nrCharsTyped;
 
   // show number of charaters left
   counterEl.textContent = charsLeft;
@@ -21,7 +49,15 @@ const inputHandler = () => {
 
 textareaEl.addEventListener("input", inputHandler);
 
-// -- Submit Component --
+// -- Form Component --
+const showVisualIndicator = (textCheck) => {
+  const className = textCheck === "valid" ? "form--valid" : "form--invalid";
+
+  //show  indicator
+  formEl.classList.add(className);
+  //remove indicator
+  setTimeout(() => formEl.classList.remove(className), 2000);
+};
 
 const submitHandler = (event) => {
   // preven default broser action submit
@@ -32,15 +68,9 @@ const submitHandler = (event) => {
 
   //validate text (ex. check if is #hastag i present and text is long enoiugh)
   if (text.includes("#") && text.length >= 5) {
-    //show valid indicator
-    formEl.classList.add("form--valid"); //remove visual indicator
-    setTimeout(() => formEl.classList.remove("form--valid"), 2000);
+    showVisualIndicator("valid");
   } else {
-    //show invalid indicator
-    formEl.classList.add("form--invalid");
-    //remove visual indicator
-    setTimeout(() => formEl.classList.remove("form--invalid"), 2000);
-
+    showVisualIndicator("invalid");
     //focus textarea
     textareaEl.focus();
 
@@ -55,26 +85,33 @@ const submitHandler = (event) => {
   const upvoteCount = 0;
   const daysAgo = 0;
 
-  // new feeback item html
-  const feebackItemHTML = `
-    <li class="feedback">
-      <button class="upvote">
-          <i class="fa-solid fa-caret-up upvote__icon"></i>
-          <span class="upvote__count">${upvoteCount}</span>
-      </button>
-      <section class="feedback__badge">
-          <p class="feedback__letter">${badgeLetter}</p>
-      </section>
-      <div class="feedback__content">
-          <p class="feedback__company">${company}</p>
-          <p class="feedback__text">${text}</p>
-      </div>
-      <p class="feedback__date">${daysAgo === 0 ? "NEW" : `${daysAgo}d`}</p>
-  </li>
-  `;
+  // render feeback and list
+  const feedbackItem = {
+    upvoteCount: upvoteCount,
+    company: company,
+    badgeLetter: badgeLetter,
+    daysAgo: daysAgo,
+    text: text,
+  };
+  renderFeedbackItem(feedbackItem);
 
-  //insert new feedback item in list
-  feebackListEl.insertAdjacentHTML("beforeend", feebackItemHTML);
+  //send feedback item to server
+  fetch(`${BASE_API_URL}/feedbacks`, {
+    method: "POST",
+    body: JSON.stringify(feedbackItem),
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        console.log("Something Went Wrong");
+        return;
+      }
+      console.log("Succesfully Submitted");
+    })
+    .catch((error) => console.log(error.message));
 
   //clear textare
   textareaEl.value = "";
@@ -83,7 +120,43 @@ const submitHandler = (event) => {
   submitBtnEl.blur();
 
   //reset counter
-  counterEl.textContent = 150;
+  counterEl.textContent = MAX_CHARS;
 };
 
 formEl.addEventListener("submit", submitHandler);
+
+// feeback list component
+const clickhandler = (event) => {
+  const clickedEl = event.target;
+
+  const upvoteIntention = clickedEl.className.includes("upvote");
+
+  if (upvoteIntention) {
+    const upvoteBtnEl = clickedEl.closest(".upvote");
+
+    upvoteBtnEl.disabled = true;
+
+    const upvoteCountEl = upvoteBtnEl.querySelector(".upvote__count");
+
+    let upvoteCounts = +upvoteCountEl.textContent;
+    upvoteCounts = upvoteCounts + 1;
+
+    upvoteCountEl.textContent = upvoteCounts++;
+  } else {
+    clickedEl.closest(".feedback").classList.toggle("feedback--expand");
+  }
+};
+
+feebackListEl.addEventListener("click", clickhandler);
+
+fetch(`${BASE_API_URL}/feedbacks`)
+  .then((response) => response.json())
+  .then((data) => {
+    spinnerEl.remove();
+    data.feedbacks.map((feedbackItem) => {
+      renderFeedbackItem(feedbackItem);
+    });
+  })
+  .catch((error) => {
+    feebackListEl.textContent = `Failed to fetch feedback list element ${error.message}`;
+  });
